80a02f96935cae0edce309e548909f4f5364ee26
Daniel Gruitt <danielgruitt@Daniels-MBP.wifi2.quayside.net.generatorhub.co.uk>
2024-01-15 15:22:24 +0000

    added app.use in app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { getHealthCheck, getAllTopics } = require("./controllers/topics.controller");
+const {
+  getHealthCheck,
+  getAllTopics,
+} = require("./controllers/topics.controller");
 
 const app = express();
 
-app.get("/api/healthCheck",getHealthCheck );
+app.get("/api/healthCheck", getHealthCheck);
 
 app.get("/api/topics", getAllTopics);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: "Server Error, please try again" });
+});
 
 module.exports = app;
