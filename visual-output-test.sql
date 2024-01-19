faf36b91583d8b46a1f2d388debffaa2854948c6
Daniel Gruitt <danielgruitt@Daniels-MBP.wifi.quayside.net.generatorhub.co.uk>
2024-01-19 17:32:02 +0000

    file structure updated

--- visual-output-test.sql
+++
@@ -1,7 +0,0 @@
-\c nc_news_test
-
-SELECT * FROM articles;
-
-SELECT * FROM users;
-
-SELECT * FROM topics;
--- visual-output.sql
+++
@@ -1,7 +0,0 @@
-\c nc_news
-
-SELECT * FROM articles;
-
-SELECT * FROM users;
-
-SELECT * FROM topics;
