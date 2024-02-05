c9f59de387344fc5d4a406faf8bba6c484301cd0
Daniel Gruitt <danielgruitt@Daniels-MacBook-Pro.local>
2024-02-05 09:40:14 +0000

    added new endpoints to jspn file for api

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ app.use(express.json());
 
 app.get("/api/healthCheck", getHealthCheck);
 
-app.get("/api/topics", getAllTopics);
-
 app.get("/api", getEndpoints);
 
-app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/topics", getAllTopics);
 
 app.get("/api/articles", getAllArticles);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.get("/api/articles/:article_id/comments", getAllCommentsByArticleId);
 
